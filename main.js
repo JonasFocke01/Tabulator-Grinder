67d18f3b04a00751415a97cb7ebc6b31a28e0cda
jonas <jonas-focke@mailbox.org>
2022-06-01 22:13:03 +0200

    implemented tab closing logic

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
-async function discardTabs() {
-  const minTabsOpen = 12;
-  const lastActiveLimit = Date.now() - options.time;
+const minTabsOpen = 6;
+const intervalInMs = 900000;
 
+async function discardTabs() {
   let tabs = await browser.tabs.query({
     active: false, // Don't discard the current tab
-    pinned: false,
-    url: ["123", "456"],
+    pinned: false, // Don't discard pinned tabs
   });
 
-  tabs = tabs.filter(function (tab) {
-    // TODO: how is the time working?
-    if (tab.lastAccessed > lastActiveLimit) {
-      return false;
-    }
-
-    return true;
+  tabs = tabs.sort(function (a, b) {
+    return a.lastAccessed - b.lastAccessed;
   });
 
-  for (const tab of tabs) {
-    //   TODO: logic needs maintanence
-    if (tabs.length > minTabsOpen) {
-      browser.tabs.discard(tab.id);
-    }
+  if (minTabsOpen < tabs.length) {
+    browser.tabs.remove(tabs[0].id);
   }
+
+  //close all New Tabs after a few seconds (no time specifyed, because the active window will never close, so you can let it be open forever)
+  tabs.forEach((tab) => {
+    if (tab.title === "New Tab") {
+      browser.tabs.remove(tab.id);
+    }
+  });
 }
 
-// setInterval(discardTabs, 60000);
-
-document.body.style.border = "5px solid red";
+setInterval(discardTabs, intervalInMs);
