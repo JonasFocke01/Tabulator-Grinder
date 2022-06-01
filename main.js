1ec79fdd6f4d5d0b5dd484e1373d604e8b1ffaf2
jonas <jonas-focke@mailbox.org>
2022-06-01 18:15:51 +0200

    added main.js -> its doing nothing right now

---
+++ main.js
@@ -0,0 +1,28 @@
+async function discardTabs() {
+  const minTabsOpen = 12;
+  const lastActiveLimit = Date.now() - options.time;
+
+  let tabs = await browser.tabs.query({
+    active: false, // Don't discard the current tab
+    pinned: false,
+    url: ["123", "456"],
+  });
+
+  tabs = tabs.filter(function (tab) {
+    // TODO: how is the time working?
+    if (tab.lastAccessed > lastActiveLimit) {
+      return false;
+    }
+
+    return true;
+  });
+
+  for (const tab of tabs) {
+    //   TODO: logic needs maintanence
+    if (tabs.length > minTabsOpen) {
+      browser.tabs.discard(tab.id);
+    }
+  }
+}
+
+setInterval(discardTabs, 60000);
