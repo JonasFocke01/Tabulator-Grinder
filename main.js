08e824c8c57a5996a9de10051a760192cfc98007
jonas <jonas-focke@mailbox.org>
2022-06-03 21:38:01 +0200

    display the current amount of tabs beside icon

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const defaultOptions = {
   useDynamicInterval: false
 };
 
+let windowId = 0;
+
 async function grindTabs() {
   const userOptions = await browser.storage.local.get();
   const options = {
@@ -17,28 +19,34 @@ async function grindTabs() {
     active: false, // Don't discard the current tab
     pinned: false, // Don't discard pinned tabs
     audible: false, // Don't discard audible tabs
+    currentWindow: true, //Only get tabs from the active window
   });
 
+  //sort tab array
   tabs = tabs.sort((a, b) => {
     return a.lastAccessed - b.lastAccessed;
   });
 
+  //discard tabs
   if (options.discardAmount > 0) {
     for (let i = 0; i < options.discardAmount * tabs.length; i++) {
       browser.tabs.discard(tabs[i].id);
     }
   }
 
+  //close tabs
   if (options.tabsToKeepOpen < tabs.length) {
     browser.tabs.remove(tabs[0].id);
   }
 
+  //close all New Tabs
   tabs.forEach((tab) => {
     if (options.closeNewTabs && tab.title === "New Tab") {
       browser.tabs.remove(tab.id);
     }
   });
 
+  //recursion
   setTimeout(() => {
     grindTabs();
   }, options.useDynamicInterval ? Math.floor(nextIntervalLength(tabs.length, 1, 30, 1500000, 0)) : options.frequency);
@@ -51,3 +59,14 @@ function nextIntervalLength(x, in_min, in_max, out_min, out_max) {
 }
 
 grindTabs();
+
+browser.windows.onFocusChanged.addListener(async (e) => {
+  const tabs = await browser.tabs.query({
+    currentWindow: true, //Only get tabs from the active window
+  });
+
+  if (e){
+    browser.browserAction.setBadgeText({text: tabs.length + '', windowId: e})
+    browser.browserAction.setBadgeBackgroundColor({color: 'green'})  
+  }
+})
